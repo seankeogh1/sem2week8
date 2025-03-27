d478553aa72e03ee98c6f03aa4e1891e659b7ceb
seankeogh1 <G00432475@atu.ie>
2025-03-27 17:37:55 +0000

    Added ,Subtract , Divide and Multiply

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -8,4 +8,26 @@ public class Calculator {
             }
             return (int) result;
         }
+        public int subtract(int firstNum, int secondNum){
+            long result = (long) firstNum - secondNum;
+            if(result < Integer.MIN_VALUE){
+                throw new IllegalArgumentException("This value was too Small");
+            }
+            return (int) result;
+        }
+
+        public int multiply(int firstNum, int secondNum){
+            long result = (long) firstNum * secondNum;
+            if(result > Integer.MAX_VALUE){
+                throw new IllegalArgumentException("This value was too big");
+            }
+            return (int) result;
+        }
+        public int divide(int firstNum, int secondNum){
+            long result = (long) firstNum / secondNum;
+            if(result > Integer.MAX_VALUE){
+                throw new IllegalArgumentException("This value was invalid");
+            }
+            return (int) result;
+        }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,5 +1,7 @@
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
@@ -9,7 +11,7 @@ public class CalculatorTest {
 
 
     @BeforeEach
-    void setup()
+     void setup()
     {
 
         myCalc = new Calculator();
@@ -23,6 +25,34 @@ public class CalculatorTest {
     void testAddFail(){
          Exception ex = assertThrows(IllegalArgumentException.class, ()-> {myCalc.add(Integer.MAX_VALUE,4);});
         assertEquals("this value is too big", ex.getMessage());
+    }
+    @Test
+    void testSubtractSuccess(){
+        assertEquals(0,myCalc.subtract(4,4));
+    }
 
+    @Test
+    void testSubtractFail(){
+        Exception ex = assertThrows(IllegalArgumentException.class, ()-> {myCalc.subtract(Integer.MIN_VALUE,4);});
+        assertEquals("this value is too small", ex.getMessage());
+    }
+    @Test
+    void testMultiplySuccess(){
+        assertEquals(16,myCalc.multiply(4,4));
+    }
+
+    @Test
+    void testMultiplyFail(){
+        Exception ex = assertThrows(IllegalArgumentException.class, ()-> {myCalc.multiply(Integer.MAX_VALUE,4);});
+        assertEquals("this value is too big", ex.getMessage());
+    }
+    @Test
+    void testDivideSuccess(){
+        assertEquals(1,myCalc.divide(4,4));
+    }
+    @Test
+    void testDivideFail(){
+        Exception ex = assertThrows(IllegalArgumentException.class, ()-> {myCalc.divide(Integer.MAX_VALUE,0);});
+        assertEquals("this value is invalid", ex.getMessage());
     }
 }
